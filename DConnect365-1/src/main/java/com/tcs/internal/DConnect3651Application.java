83b18ee84175691969dada17db66710a05cf5730
Srinath <Srinath@IP-C0A8C2DE.corp.amazonworkspaces.com>
2019-02-12 07:21:54 +0000

    Hope it works

---
+++ DConnect365-1/src/main/java/com/tcs/internal/DConnect3651Application.java
@@ -0,0 +1,14 @@
+package com.tcs.internal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DConnect3651Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DConnect3651Application.class, args);
+	}
+
+}
+
---
+++ DConnect365-1/src/test/java/com/tcs/internal/DConnect3651ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tcs.internal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DConnect3651ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
