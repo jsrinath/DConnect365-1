be5603dceaa89959255d63d6c7d4c80e1bc6aca2
Srinath <Srinath@IP-C0A8C2DE.corp.amazonworkspaces.com>
2019-02-14 13:02:40 +0000

    mongDb

--- DConnect365-1/src/main/java/com/tcs/internal/controller/HomeController.java
+++ DConnect365-1/src/main/java/com/tcs/internal/controller/HomeController.java
@@ -1,19 +1,31 @@
 package com.tcs.internal.controller;
 
 import java.net.Authenticator.RequestorType;
+import java.util.List;
 
 import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tcs.internal.models.BookingDetails;
 
 @Controller
 public class HomeController {
+	@Autowired
+	com.tcs.internal.repo.bookingRepo bookingRepo;
+	
 	@RequestMapping("/index")
 	public String getIndex() {
-		System.out.println("test");
+		  BookingDetails bd = bookingRepo.findBypatientName("Srinath");
+		  List<BookingDetails> bdList = bookingRepo.findAll();
+		  System.out.println(bdList.get(0).get_id()+""+bdList.get(0).getPatientName());
+		  System.out.println("test"+bd.getPatientName()+bd.getBookingDate()+bd.getDoctorName());
+		 
 		return "hello.html";
 	}
 	
@@ -24,9 +36,11 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value="/PatientHist",method=RequestMethod.GET)
-	public String getPatientHistory() {
+	public ModelAndView getPatientHistory() {
 		System.out.println("home");
-		return "index.html";
+		List<BookingDetails> bdList = bookingRepo.findAll();
+		ModelAndView mv =new ModelAndView("index.html","bdList",bdList);
+		return mv;
 	}
 	
 	@RequestMapping("/mdTable")
