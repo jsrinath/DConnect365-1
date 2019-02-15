6baaf9969f67bc745d92dacda98312b35d1d7534
Q652893a <Q652893a@cskpcloudwn1232.cloud.kp.org>
2019-02-15 04:18:06 -0500

    Test

--- DConnect365-1/src/main/java/com/tcs/internal/controller/HomeController.java
+++ DConnect365-1/src/main/java/com/tcs/internal/controller/HomeController.java
@@ -1,24 +1,33 @@
 package com.tcs.internal.controller;
 
 import java.net.Authenticator.RequestorType;
+import java.util.List;
 
 import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tcs.internal.models.BookingDetails;
 
 @Controller
 public class HomeController {
+	@Autowired
+	bookingRepo bookingRepo;
+	
 	@RequestMapping("/index")
 	public String getIndex() {
-		System.out.println("test");
+		  BookingDetails bd = bookingRepo.findBypatientName("Srinath");
+		  List<BookingDetails> bdList = bookingRepo.findAll();
+		  System.out.println(bdList.get(0).get_id()+""+bdList.get(0).getPatientName());
+		  System.out.println("test"+bd.getPatientName()+bd.getBookingDate()+bd.getDoctorName());
+		 
 		return "hello.html";
 	}
 	
@@ -27,21 +36,13 @@ public class HomeController {
 		System.out.println("home");
 		return "Mainpage.html";
 	}
-	@RequestMapping(value="/saveAppointment",method=RequestMethod.GET)
-	@ResponseBody
-	public ResponseEntity<String> bookAppointment(@RequestParam("deptName") String deptName,@RequestParam("drName") String drName,
-			@RequestParam("dateSel") String dateSel,@RequestParam("timeSel") String timeSel) {
-		System.out.println("Test" + deptName + drName + dateSel+timeSel);
-		BookingDetails bd = new BookingDetails();
-		
-		return new ResponseEntity<String>("{\"result\":\"success\"}", HttpStatus.OK);
-		 
-	}
 	
 	@RequestMapping(value="/PatientHist",method=RequestMethod.GET)
-	public String getPatientHistory() {
+	public ModelAndView getPatientHistory() {
 		System.out.println("home");
-		return "index.html";
+		List<BookingDetails> bdList = bookingRepo.findAll();
+		ModelAndView mv =new ModelAndView("index.html","bdList",bdList);
+		return mv;
 	}
 	
 	@RequestMapping("/mdTable")
