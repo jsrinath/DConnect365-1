7dc5d5647adb5c494fe905f6d7189d713e0e4b16
Q652893a <Q652893a@cskpcloudwn1232.cloud.kp.org>
2019-02-15 04:12:12 -0500

    UI

--- DConnect365-1/src/main/java/com/tcs/internal/controller/HomeController.java
+++ DConnect365-1/src/main/java/com/tcs/internal/controller/HomeController.java
@@ -4,10 +4,15 @@ import java.net.Authenticator.RequestorType;
 
 import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tcs.internal.models.BookingDetails;
 
 @Controller
 public class HomeController {
@@ -21,6 +26,16 @@ public class HomeController {
 	public String alertmMe() {
 		System.out.println("home");
 		return "Mainpage.html";
+	}
+	@RequestMapping(value="/saveAppointment",method=RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<String> bookAppointment(@RequestParam("deptName") String deptName,@RequestParam("drName") String drName,
+			@RequestParam("dateSel") String dateSel,@RequestParam("timeSel") String timeSel) {
+		System.out.println("Test" + deptName + drName + dateSel+timeSel);
+		BookingDetails bd = new BookingDetails();
+		
+		return new ResponseEntity<String>("{\"result\":\"success\"}", HttpStatus.OK);
+		 
 	}
 	
 	@RequestMapping(value="/PatientHist",method=RequestMethod.GET)
